8b171677c7a06f1f45052c9fd938b5747e63dcbe
Carolina <95381350+carolinamunozce@users.noreply.github.com>
2023-04-02 23:33:59 -0400

    Delete OneDrive/Escritorio/Aprendiendo Python/python_basic_learning directory

--- OneDrive/Escritorio/Aprendiendo Python/python_basic_learning/conversor_pesos_dolares.py
+++
@@ -1,7 +0,0 @@
-pesos = input("Cuantos pesos chilenos tiene: ")
-pesos = float(pesos)
-valor_dolar = 792.61
-dolares = pesos / valor_dolar
-dolares = round(dolares,2)
-dolares = str(dolares)
-print ('Tienes $ ' + dolares + ' dolares')
